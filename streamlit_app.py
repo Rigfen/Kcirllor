d1a5b9501e4f6cfeb7c3e8ed2852d22f7f2d39fd
Rigfen <Rigfen@gmail.com>
2025-12-18 13:11:44 -0800

    Update button styles and video link in app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,12 @@ st.markdown("""
 <style>
 div.stButton > button {
     width: 100%;
-    height: 120px;
-    font-size: 32px;
-    font-weight: bold;
+    height: 160px;
+    font-size: 40px;
+    border-radius: 16px;
 }
 </style>
 """, unsafe_allow_html=True)
 
-if st.button("▶ WATCH VIDEO"):
-    st.video("https://www.youtube.com/watch?v=YOUR_VIDEO_ID")
+if st.button("Just a little app"):
+    st.video("https://www.youtube.com/watch?v=xvFZjo5PgG0")
